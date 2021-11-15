8b9ea34c1ae53c9db880657bb3fca9f69b6536c6
dmoon4117 <dmoon4117@gmail.com>
2021-11-15 22:21:58 +0900

    11/15 2주차 LayoutCodeLab

--- week 2-1-Layouts in JetpackCompose/GDGLayoutCodeLab/app/src/main/java/com/codelab/gdglayoutcodelab/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/GDGLayoutCodeLab/app/src/main/java/com/codelab/gdglayoutcodelab/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     PhotographerCard()
-                }
+                 }
             }
         }
     }
